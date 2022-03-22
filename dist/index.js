f1c75d3a2d125d7e72fb0b2b66caa4fc533a15bd
Ankur Atri <ankuratri.in@gmail.com>
2022-03-22 17:57:44 +0530

    Basic Types, Array and Tuples added

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,28 @@
 "use strict";
+/**
+ * Basic Types
+ */
 let count = 5;
-console.log(count);
+let company = "asquare";
+let isPublished = true;
+console.log({ count, company, isPublished });
+let address = {
+    city: "Gurgaon",
+    state: "Haryana",
+};
+/**
+ * Arrays & Tuples
+ */
+let ids = [1, 2, 3, 4, 5];
+// Gives Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// ids.push("hello");
+let arr = [true, {}, 1, undefined, null, []];
+// Tuple
+let person = ["Ankur", 27, true];
+// Tuple Array
+let employees;
+employees = [
+    [1, "Ankur"],
+    [2, "Rajat"],
+    [3, "Mohit"],
+];
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,36 @@
+/**
+ * Basic Types
+ */
+
 let count: number = 5;
-console.log(count);
+let company: string = "asquare";
+let isPublished: boolean = true;
+
+console.log({ count, company, isPublished });
+
+let address: any = {
+  city: "Gurgaon",
+  state: "Haryana",
+};
+
+/**
+ * Arrays & Tuples
+ */
+
+let ids: number[] = [1, 2, 3, 4, 5];
+// Gives Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+// ids.push("hello");
+
+let arr: any[] = [true, {}, 1, undefined, null, []];
+
+// Tuple
+let person: [string, number, boolean] = ["Ankur", 27, true];
+
+// Tuple Array
+let employees: [number, string][];
+
+employees = [
+  [1, "Ankur"],
+  [2, "Rajat"],
+  [3, "Mohit"],
+];
