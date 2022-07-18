95cc4c74a464d0c16512dc9c072fca973650fc9f
Ankur Atri <ankuratri.in@gmail.com>
2022-07-18 15:30:23 +0530

    union type example added

--- dist/advanced-types.js
+++ dist/advanced-types.js
@@ -9,4 +9,19 @@ let securityGuard = {
         console.log(date);
     },
 };
+/**
+ * Union Types
+ */
+function kgToLbs(weight) {
+    // Narrowing
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+const myWeight = kgToLbs(88);
+const myFriendWeight = kgToLbs("75Kg");
+console.log({ myWeight, myFriendWeight });
 //# sourceMappingURL=advanced-types.js.map
--- src/advanced-types.ts
+++ src/advanced-types.ts
@@ -15,3 +15,21 @@ let securityGuard: WorkMan = {
     console.log(date);
   },
 };
+
+/**
+ * Union Types
+ */
+
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+  if (typeof weight === "number") {
+    return weight * 2.2;
+  } else {
+    return parseInt(weight) * 2.2;
+  }
+}
+
+const myWeight = kgToLbs(88);
+const myFriendWeight = kgToLbs("75Kg");
+
+console.log({ myWeight, myFriendWeight });
