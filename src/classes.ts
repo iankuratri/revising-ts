6ea2145e161b427130d0b6597910e35cc8e26778
Ankur Atri <ankuratri.in@gmail.com>
2022-07-20 22:47:00 +0530

    static member exmaple added

--- src/classes.ts
+++ src/classes.ts
@@ -43,3 +43,31 @@ class SeatAssignment {
 let seats = new SeatAssignment();
 seats.A1 = "Mosh";
 seats.A2 = "Jhon";
+
+/**
+ * Static Members
+ */
+
+class Ride {
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+
+  stop() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides(): number {
+    return Ride._activeRides;
+  }
+}
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
